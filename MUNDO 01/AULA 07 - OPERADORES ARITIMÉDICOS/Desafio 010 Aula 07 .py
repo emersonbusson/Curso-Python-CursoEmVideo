55b3271329e9f879896f915126a60a1c7e3c84ec
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-26 15:29:58 -0300

    mudanças no código

--- MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 010 Aula 07 .py
+++ MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 010 Aula 07 .py
@@ -1,10 +1,10 @@
- #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+#Desafio 010 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
-real = float(input('Digite quanto dinheiro você tem na carteira: '))
-dolar = 5.53
-eur = 6.40
-cart = real / dolar
-cart2 = real / eur
+real = float(input('Digite quantos R$(Reias) tem na carteira: '))
+preco_dolar = 5.21
+preco_euro = 5.54
+cartateira_real_dolar = real / preco_dolar
+cartateira_real_euro = real / preco_euro
 print('___'*12)
-print('Com R$ {:.2f} na carteira! \n Você pode comprar: \n US$: {:.2f} \n €: {:.2f}'.format(real,cart, cart2))
+print('Com R$ {:.2f} na carteira!\nVocê pode comprar:\nUS$: {:.2f}\n€: {:.2f}'.format(real,cartateira_real_dolar,cartateira_real_euro))
 print('___'*12) 
