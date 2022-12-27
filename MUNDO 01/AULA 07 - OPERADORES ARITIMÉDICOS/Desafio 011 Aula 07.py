bac30b47b3e40ab968c62cbd25bb09e14ea15c40
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-27 10:35:24 -0300

    refactoring

--- MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 011 Aula 07.py
+++ MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 011 Aula 07.py
@@ -1,8 +1,7 @@
-# Faça um programa que leia a largura e altura de uma parede em metros, calcule a sua aréa e quantidade de tinta nescessária para pinta-ló
-# sabendo que cada litro de tinta pinta uma aréa de 2m2.
+#Desafio 011 - Faça um programa que leia a largura e altura de uma parede em metros, calcule a sua área e quantidade de tinta necessária para pinta-ló, sabendo que cada litro de tinta pinta uma área de 2m2.
 
-larg = float(input('Digite a largura da parede:'))
-alt = float(input('Digite a altura da parede: '))
-area = larg * alt
-print('Sua parede tem a dimensão de {}X{} e Sua aréa pe de {}m²'.format(larg,alt,area))
-print('Você vai precisar de {} litros de tinta para pintar essa parede!'.format(area / 2))
+largura_parede = float(input('Digite a largura da parede em metros: '))
+altura_parede = float(input('Digite a altura da parede em metros: '))
+area_parede = largura_parede * altura_parede
+print('A sua parede tem a dimensão de {} X {} e a sua área é de: {}m²'.format(largura_parede,altura_parede,altura_parede))
+print('Você vai precisar de {} litros de tinta para pintar essa parede!'.format(altura_parede / 2))
