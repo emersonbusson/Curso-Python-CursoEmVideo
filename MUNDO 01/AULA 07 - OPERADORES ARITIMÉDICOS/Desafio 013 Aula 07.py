d9c522568bef657d54a00952fdc911f58030a474
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-27 11:04:24 -0300

    refactoring.

--- MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 013 Aula 07.py
+++ MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 013 Aula 07.py
@@ -1,5 +1,5 @@
-''' Faça um algoritimo que leia o salário de um funcionario e mostre seu novo sálario, com 15% de aumento'''
+# Desafio 013 - Faça um algorítimo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento.
 
-sala = float(input('Qual é o sálario do funcionario? R$: '))
-novo = sala + (sala * 15 /100)
-print('Um Funcionario que ganhava R$: {}, com 15% de aumento, passa a receber R$: {}'.format(sala, novo))
+salario_funcionario = float(input('Qual é o sálario do funcionario? R$: '))
+salario_15aumento = salario_funcionario * 0.15
+print(f'Um Funcionario que ganhava R$: {salario_funcionario:6.2f}.\nCom 15% de aumento R$ {salario_15aumento:6.2f}.\nAgora passa a receber R$: {salario_funcionario + salario_15aumento:6.2f}.')
