6c25130251d8cea596b960751f665a8e4bfb88a6
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-27 10:51:45 -0300

    reestruturação.

--- MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 012 Aula 07.py
+++ MUNDO 01/AULA 07 - OPERADORES ARITIMÉDICOS/Desafio 012 Aula 07.py
@@ -1,8 +1,7 @@
-#Faça um algoritimo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+#Desafio 012 - Faça um algorítimo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
 
-preço = float(input('Digite o preço do produto:'))
-calc = preço *5 / 100
-desc = calc
-result = preço - calc
+preco_produto = float(input('Digite o preço do produto: '))
+desconto_produto = preco_produto1 * 5 / 100
+novo_preco_desconto = preco_produto - desconto_produto
 
-print('Sua compra no valor de R$ {:.2f}. \n Com um desconto de 5% que é de: R$ {:.2f}. \n Fica um total de R$ {:.2f} com o desconto.'.format(preço,calc,result))
+print('Sua compra no valor de R$ {:.2f}.\nO desconto de 5% é de R${:.2f}.\nFica um total de R$ {:.2f} com o desconto aplicado.'.format(preco_produto,desconto_produto,novo_preco_desconto))
