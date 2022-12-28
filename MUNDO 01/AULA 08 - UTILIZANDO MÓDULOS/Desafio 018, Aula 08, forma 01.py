fca2fdc2b8d856bc98395597bb9b6f3d73a7e1cc
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-28 16:05:40 -0300

    refactoring

--- MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 018, Aula 08, forma 01.py
+++ MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 018, Aula 08, forma 01.py
@@ -1,15 +1,14 @@
-#Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno, cosceno e tangente.
+#Desafio 018 - Faça um programa que leia um angulo qualquer e mostre na tela os valores do seno, cosseno e tangente.
 
 import math
 
-user_digitado = float(input('Digite um angulo qualquer:'))
-angu = user_digitado
-angu = math.radians(angu)
-sen01 = math.sin(angu)
-cos01 = math.cos(angu)
-tang01 = math.tan(angu)
+angulo = float(input('Digite um angulo qualquer: '))
+angulo_radiano = math.radians(angulo)
+seno = math.sin(angulo_radiano)
+cosseno = math.cos(angulo_radiano)
+tangente = math.tan(angulo_radiano)
 
 
-print('O Ângulo de: {:.1f} , tem o SENO de: {:.2f},'.format(user_digitado, sen01))
-print('O Ângulo de: {:.1f} , tem o COSSENO de: {:.2f}'.format(user_digitado,cos01))
-print('O Ângulo de: {:.1f} , tem a TANGENTE de: {:.2f}'.format(user_digitado,tang01))
+print('O Ângulo de: {:.1f}° , tem o SENO de: {:.2f}'.format(angulo, seno))
+print('O Ângulo de: {:.1f}° , tem o COSSENO de: {:.2f}'.format(angulo,cosseno))
+print('O Ângulo de: {:.1f}° , tem a TANGENTE de: {:.2f}'.format(angulo,tangente))
--- MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 018, Aula 08, forma 02.py
+++ MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 018, Aula 08, forma 02.py
@@ -1,11 +1,11 @@
+#Desafio 018 - Faça um programa que leia um angulo qualquer e mostre na tela os valores do seno, cosseno e tangente.
+
 from math import radians, sin, cos, tan
-angu = float(input('Digite o Ângulo Desejado:'))
-seno = sin(radians(angu))
-coss = cos(radians(angu))
-tan1 = tan(radians(angu))
-print('O Ângulo de: {:.2f}, tem o SENO de: {:.2f}'.format(angu,seno))
-print('O Ângulo de: {:.2f}, tem o COSSENO de: {:.2f}'.format(angu,coss))
-print('O Ângulo de: {:.2f}, tem a TANGENTE de: {:.2f}'.format(angu, tan1))
 
-
-#Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno, cosceno e tangente.
+angulo = float(input('Digite o Ângulo Desejado: '))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+print(f'O Ângulo de: {angulo:.2f}, tem o SENO de: {seno:.2f}')
+print(f'O Ângulo de: {angulo:.2f}, tem o COSSENO de: {cosseno:.2f}')
+print(f'O Ângulo de: {angulo:.2f}, tem a TANGENTE de: {tangente:.2f}')
