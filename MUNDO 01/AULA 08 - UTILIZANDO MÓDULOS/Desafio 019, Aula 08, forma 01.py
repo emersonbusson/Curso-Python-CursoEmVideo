71eff8906a3fea9ae91e668296e735f1085c6607
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-28 20:19:48 -0300

    refactoring

--- MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 019, Aula 08, forma 01.py
+++ MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 019, Aula 08, forma 01.py
@@ -1,9 +1,13 @@
-# Desafio 019 ~ O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos, faça um programa que leia o nome dos quarto alunos e mostre a ordem sorteada.
+# Desafio 019 - O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos, faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
 import random
-a1 = str(input('Digite o nome do primeiro aluno:'))
-a2 = str(input('Digite o nome do segundo aluno:'))
-a3 = str(input('Digite o nome do terceiro aluno:'))
-a4 = str(input('Digite o nome do quarto aluno:'))
-lista = [a1,a2,a3,a4]
-lista = random.sample(lista,len(lista))
-print('A lista sequencial de alunos sorteados é: {} '.format(lista))
+
+aluno_1 = str(input('Digite o nome do primeiro aluno: '))
+aluno_2 = str(input('Digite o nome do segundo aluno: '))
+aluno_3 = str(input('Digite o nome do terceiro aluno: '))
+aluno_4 = str(input('Digite o nome do quarto aluno: '))
+lista_sorteio = [aluno_1,aluno_2,aluno_3,aluno_4]
+sorteados = random.sample(lista_sorteio,len(lista_sorteio))
+
+for indice, aluno in enumerate(sorteados):
+    print('O a ordem de apresentação é: {}° - {} '.format(indice + 1, aluno))
