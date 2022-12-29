3bebf8854d78644a3f8f2cdaba16f8a7b939dafe
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-29 16:56:12 -0300

    i'm Refactored certain parts of the code to improve its readability and maintainability.

--- MUNDO 01/AULA 10 - Condições (Parte 1)/Desafio 030, Aula 10, forma 01.py
+++ MUNDO 01/AULA 10 - Condições (Parte 1)/Desafio 030, Aula 10, forma 01.py
@@ -1,15 +1,10 @@
-'''Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.'''
+# Desafio 030 - Crie um programa que leia um número inteiro e mostre na tela se ele é par ou ímpar.
 import time
 
 
-
-print('____' *20)
-numero = int(input('Digite um número inteiro e te direis se ele é par ou ímpar:'))
-print('____' *20)
-time.sleep(2)
+numero = int(input('Digite um número inteiro e te direis se ele é par ou ímpar: '))
+time.sleep(0.5)
 print('Processando...')
-
-print('____'*20)
 
 if (numero%2) == 0:
     print('O número: {}, é PAR.'.format(numero))
--- MUNDO 01/AULA 10 - Condições (Parte 1)/Desafio 031, Aula , Forma 01.py
+++ MUNDO 01/AULA 10 - Condições (Parte 1)/Desafio 031, Aula , Forma 01.py
@@ -1,25 +1,16 @@
-'''DESENVOLVA UM PROGRAMA QUE PERGUNTE A DISTANCIA DE UMA VIAGEM EM KM.'''
-
-
-'''CALCULE O PREÇO DA PASSAGEM, COBRANDO R$ 0,50, POR KM, PARA VIAGENS DE ATÉ 200KM, E R$ 0,45, PARA VIAGENS MAIS LONGAS.'''
+# Desafio 031 - Desenvolva um programa que pergunte a distância de uma viagem em km. Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de até 200 km e R$ 0,45 para viagens mais longas.
 
 import time
-print('___' *20)
-kmdiguser = float(input('Digite a quantidade de KM da Sua Viagem:'))
-print('___' *20)
-print('CALCULANDO O VALOR A SER PAGO POR "KM", POR FAVOR, AGUARDE...')
-time.sleep(2)
-print('___' *20)
 
-km200 = 0.50
-kmmais200 = 0.45
+distancia_km = float(input('Digite a distância em kM da Sua Viagem: '))
+print('Calculando...')
+time.sleep(1.0)
+preco_menor_200km = 0.50
+preco_acima_200km = 0.45
 
-if kmdiguser <= 200 :
-    print('O valor da Viagem é de R$: {:.2f}'.format(kmdiguser * km200 ))
+if distancia_km <= 200 :
+    print('O valor por kM desta viagem é R$: {:.2f}'.format(preco_menor_200km))
+    print('O preço da passagem é: R$ {:.2f}'.format(distancia_km * preco_menor_200km))
 else:
-    print('O valor da Viagem a ser pago com desconto por .KM. devido a longa distancia é de R$ {:.2f}'.format(kmmais200 * kmdiguser))
-
-
-
-
-
+    print('O valor por kM a ser pago com desconto por longa distância: R$ {:.2f} '.format(preco_acima_200km))
+    print('O preço da passagem é: R$ {:.2f}'.format(distancia_km * preco_acima_200km))
