cdb1dcde3d4fe341817229fd30f533eb819e98e4
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-29 12:43:36 -0300

    refactoring

--- MUNDO 01/AULA 09 - MANIPULANDO CADEIAS DE TEXTO/Desafio 022, Aula  09, Forma 01.py
+++ MUNDO 01/AULA 09 - MANIPULANDO CADEIAS DE TEXTO/Desafio 022, Aula  09, Forma 01.py
@@ -1,21 +1,12 @@
-'''Crie um programa que leia o nome completo de uma pessoa e mostre:'''
+#Desafio 022 - Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas, o nome com todas as letras minúsculas, quantas letras ao todo (Sem considerar espaços), quantas letras tem o primeiro nome.
 
-'''O nome com todas as letras maiúsculas'''
-'''O nome com todas as letras minúsculas'''
-'''Quantas letras ao todo (Sem considerar espaços'''
-'''Quantas letras tem o primeiro nome'''
-
-nome = str(input("Digite o nome completo da pessoa:")).strip()
+nome = str(input("Digite o nome completo da pessoa: ")).strip()
 print('Analisando seu nome...')
 print('Seu nome em letras maiúsculas é: {}'.format(nome.upper()))
 print('Seu nome em letras minúsculas é: {}'.format(nome.lower()))
 print('Seu nome tem o total de: {} de letras.'.format(len(nome) - nome.count(' ')))
-#print('Seu primeiro nome tem: {}, letras!'.format(nome.find(' ')))
-separa = nome.split()
-print('Seu primeiro nome é: {} , e ele tem {}, letras!'.format(separa[0], len(separa[0])))
 
+# print('Seu primeiro nome tem: {}, letras!'.format(nome.find(' '))) o trecho de string " " (espaço em branco) foi encontrado na posição 7 da string nome.
 
-
-
-
-
+nome_separado_lista = nome.split() # o método split separou o nome pelos espaços (em branco) e colocou cada pedaço em uma lista.
+print('Seu primeiro nome é: {} , e ele tem {}, letras!'.format(nome_separado_lista[0], len(nome_separado_lista[0])))
