2d1380a55ab0ec1a6e997eed291d8aef9c2c694f
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2022-12-28 23:50:27 -0300

    refactoring

--- MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 021, Aula 08, forma 01.py
+++ MUNDO 01/AULA 08 - UTILIZANDO MÓDULOS/Desafio 021, Aula 08, forma 01.py
@@ -1,3 +1,5 @@
+#Desafio 021 - Faça um programa em python que abra e reproduza o áudio de um arquivo.mp3
+
 import vlc
 import time
 music = 'MUSICA KIKO.mp3'
@@ -6,5 +8,3 @@ player.play()
 print('Musica está tocando')
 time.sleep(179)
 print('MUSICA PAROU')
-
-#FAÇA UM PROGRAMA EM PYTHON QUE ABRA E REPRODUZA O ÁUDIO DE UM ARQUIVO.MP3
