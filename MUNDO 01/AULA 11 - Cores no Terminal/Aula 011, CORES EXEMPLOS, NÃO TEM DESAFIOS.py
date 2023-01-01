92d2df8e1b1ba95a4c66c2181c04170e90a53b99
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2023-01-01 18:30:45 -0300

    Refatorei algumas partes do código para melhorar sua legibilidade.

--- MUNDO 01/AULA 11 - Cores no Terminal/Aula 011, CORES EXEMPLOS, NÃO TEM DESAFIOS.py
+++ MUNDO 01/AULA 11 - Cores no Terminal/Aula 011, CORES EXEMPLOS, NÃO TEM DESAFIOS.py
@@ -1,9 +1,9 @@
-nome3 = str(input('\33[0:31:40mQUAL É O TERCEIRO NOME PARA O EXEMPLO?\33[m'))
-nome4 = str(input('\33[0:34:40mQUAL É O OUTRO NOME A SER EXIBIDO?\33[m'))
-
-nome = 'EMERSON BRUNO BUSSON'
-
+nome1 = 'EMERSON BRUNO BUSSON'
 nome2 = 'ADRIANA BRUNO MONTEIRO'
+nome3 = str(input('\33[0:31:40mQUAL É O TERCEIRO NOME PARA O EXEMPLO: \33[m'))
+nome4 = str(input('\33[0:34:40mQUAL É O OUTRO NOME A SER EXIBIDO: \33[m'))
+
+
 
 cores = {'limpa':          '\33[m',
          'azul':           '\033[4:34m',
@@ -11,7 +11,7 @@ cores = {'limpa':          '\33[m',
          'pretoebranco':   '\33[7m'
          }
 
-print('Muito prazer em te conhecer:{}{}{} '.format('\33[4:35m' ,nome, '\33[m'))
+print('Muito prazer em te conhecer:{}{}{} '.format('\33[4:35m' ,nome1, '\33[m'))
 
 ## OUTRO EXEMPLO DE CORES COM VARIAVEIS.
 
