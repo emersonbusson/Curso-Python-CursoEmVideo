f207bbe24e4cb22628d92a2403f335e2a9e34e9f
Emerson Busson <93008583+emersonbusson@users.noreply.github.com>
2023-01-09 15:54:41 -0300

    Refatorei algumas partes do código para melhorar sua legibilidade

--- MUNDO 02/AULA 013 - Estrutura de repetição for/Desafio 048, Aula 013, forma 01.py
+++ MUNDO 02/AULA 013 - Estrutura de repetição for/Desafio 048, Aula 013, forma 01.py
@@ -5,8 +5,7 @@ cont = 0
 
 for c in range (1,501,2):
   if c %3 == 0:
-    soma += c
-    cont += 1
+      soma += c
+      cont += 1
 print(f'A soma de todos os valores é: {soma}')
 print(f'Os valores solicitados São: {cont}')
-
--- MUNDO 02/AULA 013 - Estrutura de repetição for/Desafio 050, Aula 013, forma 01.py
+++ MUNDO 02/AULA 013 - Estrutura de repetição for/Desafio 050, Aula 013, forma 01.py
@@ -1,9 +1,9 @@
 # Desafio 050 -  Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
 soma = 0
-for c in range(6):
+for cont in range(6):
     print('-=' * 15)
-    numero = int(input(f'Digite o {c+1}º número inteiro: '))
+    numero = int(input(f'Digite o {cont+1}º número inteiro: '))
     if numero % 2 == 0 and numero != 0:
         soma += numero
 print(f'A soma dos números pares digitados é: {soma}')
